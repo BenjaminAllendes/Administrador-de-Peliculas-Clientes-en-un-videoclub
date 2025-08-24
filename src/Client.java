ca47701d60492922ebfb1ae8f95de6760c2674ff
Your Name <you@example.com>
2025-08-24 01:50:41 -0400

    Commit de Clientes y puml del diagrama provisional

---
+++ src/Client.java
@@ -0,0 +1,65 @@
+import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Client {
+    private int ID ;
+    private String name ;
+    private ArrayList<String> rent_list ;
+    private LinkedList<String> past_list ;
+    private ArrayList<String> recommended ;
+
+    public Client(int ID, String name){
+        this.ID = ID ;
+        this.name = name ;
+        rent_list = new ArrayList<>();
+        past_list = new LinkedList<>();
+        recommended = new ArrayList<>();
+    }
+
+    public void setID(){
+        if (ID >= 0) {
+            this.ID = ID ;
+        }
+    }
+
+    public int getID(){
+        return ID ;
+    }
+
+    public void setName(String name){
+        if (name.equals(name.toString())) {
+            this.name = name ;
+        }
+    }
+
+    public String getName(){
+        return name ;
+    }
+
+    public void setRent_list(){
+        rent_list = new ArrayList<>();
+    }
+
+    public ArrayList<String>getRent_list(){
+        return rent_list ;
+    }
+
+    public void setPast_list(){
+        past_list = new LinkedList<>();
+    }
+
+    public LinkedList<String>getPast_list(){
+        return past_list ;
+    }
+
+    public void setRecommended(){
+        recommended = new ArrayList<>();
+    }
+
+    public ArrayList<String>getRecommended(){
+        return recommended ;
+    }
+
+
+}
