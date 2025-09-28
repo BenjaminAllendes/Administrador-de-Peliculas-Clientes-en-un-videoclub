99052a709d8880b19f7ca6d5ec5a2e8d5d93d9b9
Your Name <you@example.com>
2025-09-27 23:24:48 -0300

    Listo UML y readme final.

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,7 @@ public class Main {
         SwingUtilities.invokeLater(() -> {
             Videoclub vc = new Videoclub();
 
-            /*/ --- BASE DE DATOS PRE-CARGADA ---
-            vc.addMovie(new Movie(1, "Matrix", "Acción", 2));
-            vc.addMovie(new Movie(2, "Titanic", "Romance", 3));
-            vc.addMovie(new Movie(3, "El Señor de los Anillos", "Fantasía", 5));
-            vc.addMovie(new Movie(4, "Toy Story", "Animación", 4));
-            vc.addMovie(new Movie(5, "Los Increíbles", "Animación", 0));
-*/
+
             vc.loadData();
             //System.out.println("5 películas precargadas en el sistema.");
 
